ff099bb18739dfd3ecc897ddbb98c02706c0ec83
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 15:48:25 +0200

    init

---
+++ init.lua
@@ -0,0 +1,27 @@
+require("utils")
+
+
+return {
+    mappings = {
+        n = {
+            ["<leader>f"] = utils.wrap_func("find_files")
+        },
+    },
+    funcs = {
+        find_files = {
+            func = function()
+                vim.cmd("Telescope find_files")
+            end,
+            desc = "Find files",
+        },
+    },
+    plugins = {
+        {
+            "nvim-telescope/telescope.nvim",
+            dependencies = {
+                { "nvim-lua/plenary.nvim" },
+                { "nvim-tree/nvim-web-devicons" },
+            },
+        },
+    },
+}
